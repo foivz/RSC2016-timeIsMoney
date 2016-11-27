b8b8fd1620a19f644aab1c8214770ee4283c592f
Igor Rinkovec <shiqqqi@gmail.com>
2016-11-27 06:19:21 +0100

    sad

--- web/app/Http/Controllers/Moderator/EventModerationController.php
+++ web/app/Http/Controllers/Moderator/EventModerationController.php
@@ -92,7 +92,7 @@ class EventModerationController extends Controller
     public function tiebreaker($eventId)
     {
         $event = Event::find($eventId);
-        $event->status = EventStatusEnum::STATUS_STARTED;
+        $event->status = EventStatusEnum::STATUS_FINISHED;
         $event->save();
         return view('moderator.moderation.tiebreaker')
             ->with('event', $event);
